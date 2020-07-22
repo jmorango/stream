57771c10839a9e95ddc739065cee2d8d3d1bc4cd
jmorango <joseamed@gmail.com>
2020-07-22 18:24:45 -0400

    working on pc
    Locally working application

---
+++ home.py
@@ -0,0 +1,183 @@
+from functools import wraps
+import json
+from os import environ as env
+from dotenv import load_dotenv, find_dotenv
+from imutils.video import VideoStream
+from flask import Flask , Response , request, render_template, jsonify, redirect, session, url_for
+import threading
+import datetime
+import imutils
+import time
+import cv2
+import os
+from authlib.integrations.flask_client import OAuth
+from six.moves.urllib.parse import urlencode
+from werkzeug.exceptions import HTTPException
+
+
+import constants
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+AUTH0_CALLBACK_URL = env.get(constants.AUTH0_CALLBACK_URL)
+AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
+AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
+AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
+AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
+AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
+
+
+outputFrame = None
+lock = threading.Lock()
+
+app = Flask(__name__, static_folder = 'uploads')
+
+app.secret_key = constants.SECRET_KEY
+
+oauth = OAuth(app)
+
+auth0 = oauth.register(
+    'auth0',
+    client_id=AUTH0_CLIENT_ID,
+    client_secret=AUTH0_CLIENT_SECRET,
+    api_base_url=AUTH0_BASE_URL,
+    access_token_url=AUTH0_BASE_URL + '/oauth/token',
+    authorize_url=AUTH0_BASE_URL + '/authorize',
+    client_kwargs={
+        'scope': 'openid profile email',
+    },
+)
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if 'profile' not in session:
+            return redirect('/')
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+@app.route('/home')
+def home():
+    return render_template('main.html',
+                       userinfo=session['profile'])
+
+@app.route('/stream')
+@requires_auth
+def stream():
+    #auth0.authorize_access_token()
+    return render_template('stream.html', userinfo=session['profile'])
+def detect_motion(frameCount):
+	# grab global references to the video stream, output frame, and
+	# lock variables
+	global vs, outputFrame, lock
+
+	# loop over frames from the video stream
+	while True:
+		# read the next frame from the video stream, resize it,
+		# convert the frame to grayscale, and blur it
+		frame = vs.read()
+		frame = imutils.resize(frame, width=400)
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+		# grab the current timestamp and draw it on the frame
+		timestamp = datetime.datetime.now()
+		cv2.putText(frame, timestamp.strftime(
+			"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+		with lock:
+			outputFrame = frame.copy()
+
+def generate():
+	# grab global references to the output frame and lock variables
+	global outputFrame, lock
+
+	# loop over frames from the output stream
+	while True:
+		# wait until the lock is acquired
+		with lock:
+
+			if outputFrame is None:
+				continue
+
+			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+
+			# ensure the frame was successfully encoded
+			if not flag:
+				continue
+
+		# yield the output frame in the byte format
+		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+			bytearray(encodedImage) + b'\r\n')
+
+@app.route('/video_feed')
+@requires_auth
+def video_feed():
+	# return the response generated along with the specific media
+	# type (mime type)
+	return Response(generate(),
+		mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+@app.route('/callback')
+def callback_handling():
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
+
+    session['jwt_payload'] = userinfo
+    session['profile'] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+    return redirect('/home')
+
+
+@app.route('/')
+def login():
+    return auth0.authorize_redirect(redirect_uri='http://localhost:3000/callback')
+
+
+app.config["VIDEO_UPLOADS"] = "C:\\Users\\Amed\\Desktop\\CNT4713\\uploads"
+@app.route('/static_video', methods = ["GET", "POST"])
+#@requires_auth
+def static_video():
+    if request.method == "POST":
+        if request.files:
+            video_upload = request.files["video_upload"]
+            video_upload.save(os.path.join(app.config["VIDEO_UPLOADS"],video_upload.filename))
+            return redirect(request.url)
+    return render_template('static_video.html', userinfo = session['profile'])
+
+
+@app.route('/dashboard')
+@requires_auth
+def dashboard():
+    return render_template('dashboard.html',
+                           userinfo=session['profile'],
+                           userinfo_pretty=json.dumps(session['jwt_payload'], indent=4))
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    params = {'returnTo': url_for('login', _external=True), 'client_id': AUTH0_CLIENT_ID}
+    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+
+
+t = threading.Thread(target=detect_motion, args=(32,))
+t.daemon = True
+t.start()
+
+app.run(host = "0.0.0.0", port="3000", debug=True,
+threaded=True, use_reloader=False)
+vs.stop()
