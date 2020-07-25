22907391fd9461fef34d23c0d015c917ecae36c0
jmorango <joseamed@gmail.com>
2020-07-24 23:59:13 -0400

    fixing wsgi file to run socketio

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app.home import app
+from app.home import app, socketio
 if __name__ == "__main__":
-	socketio.run(app = home, host = '0.0.0.0', port = 5001)
-    app.run()
+	socketio.run(app, host = '0.0.0.0', port = 5001)
+	app.run()
